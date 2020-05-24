f658b181a7614d83af6d92239fb4b5834b1ea481
Delfio <delfio_eu@hotmail.com>
2020-05-24 14:07:52 -0400

    adicionando controle de atualização e criação

---
+++ src/models/User.ts
@@ -0,0 +1,28 @@
+/* eslint-disable camelcase */
+import {
+  Entity, Column, PrimaryGeneratedColumn, CreateDateColumn, UpdateDateColumn,
+} from 'typeorm';
+
+export interface Iappointment {
+  id: string,
+  provider: string,
+  date: Date
+}
+@Entity('users')
+class User {
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @CreateDateColumn()
+  created_at: Date;
+
+  @UpdateDateColumn()
+  updated_at: Date;
+}
+export default User;
