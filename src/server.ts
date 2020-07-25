1a88bbd9e1bc030a63b1a1ba8a136df8d66fca20
Delfio <delfio_eu@hotmail.com>
2020-07-25 18:24:11 -0400

    add cors

--- src/server.ts
+++ src/server.ts
@@ -3,15 +3,17 @@ import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 
 import 'express-async-errors';
-
+import cors from 'cors';
 import routes from './routes';
 import './database';
 import AppError from './errors/AppError';
 import uploadConfig from './config/upload';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
+
 app.use(routes);
 
 app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
